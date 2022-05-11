d63984f150764baa5179d95932b37fbf04329562
JP Sugarbroad <taralx@gmail.com>
2022-05-10 22:42:11 -0700

    remove js-sha256

--- src/ui/React/AlertManager.tsx
+++ src/ui/React/AlertManager.tsx
@@ -3,38 +3,22 @@ import { EventEmitter } from "../../utils/EventEmitter";
 import { Modal } from "./Modal";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { sha256 } from "js-sha256";
 
 export const AlertEvents = new EventEmitter<[string | JSX.Element]>();
 
-interface Alert {
-  id: string;
-  text: string | JSX.Element;
-  hash: string;
-}
-
-let i = 0;
-export function AlertManager(): React.ReactElement {
-  const [alerts, setAlerts] = useState<Alert[]>([]);
+export function AlertManager(): React.ReactElement | null {
+  const [alerts, setAlerts] = useState<[Set<string | JSX.Element>]>([new Set()]);
   useEffect(
     () =>
       AlertEvents.subscribe((text: string | JSX.Element) => {
-        const id = i + "";
-        i++;
         setAlerts((old) => {
-          const hash = getMessageHash(text);
-          if (old.some((a) => a.hash === hash)) {
+          const [alerts] = old;
+          if (alerts.has(text)) {
             console.log("Duplicate message");
             return old;
           }
-          return [
-            ...old,
-            {
-              id: id,
-              text: text,
-              hash: hash,
-            },
-          ];
+          alerts.add(text);
+          return [alerts];
         });
       }),
     [],
@@ -43,33 +27,33 @@ export function AlertManager(): React.ReactElement {
   useEffect(() => {
     function handle(this: Document, event: KeyboardEvent): void {
       if (event.code === "Escape") {
-        setAlerts([]);
+        setAlerts([new Set()]);
       }
     }
     document.addEventListener("keydown", handle);
     return () => document.removeEventListener("keydown", handle);
   }, []);
 
-  function getMessageHash(text: string | JSX.Element): string {
-    if (typeof text === "string") return sha256(text);
-    return sha256(JSON.stringify(text.props));
+  let current: string | JSX.Element | null = null;
+  for (const alert of alerts[0]) {
+    current = alert;
+    break;
   }
 
   function close(): void {
-    setAlerts((old) => {
-      return old.slice(1, 1e99);
+    setAlerts(orig => {
+      if (current == null) return orig;
+      const [alerts] = orig;
+      alerts.delete(current);
+      return [alerts];
     });
   }
 
-  return (
-    <>
-      {alerts.length > 0 && (
-        <Modal open={true} onClose={close}>
-          <Box overflow="scroll" sx={{ overflowWrap: "break-word", whiteSpace: "pre-line" }}>
-            <Typography component={"span"}>{alerts[0].text}</Typography>
-          </Box>
-        </Modal>
-      )}
-    </>
+  return current == null ? null : (
+    <Modal open={true} onClose={close}>
+      <Box overflow="scroll" sx={{ overflowWrap: "break-word", whiteSpace: "pre-line" }}>
+        <Typography component={"span"}>{current}</Typography>
+      </Box>
+    </Modal>
   );
 }
